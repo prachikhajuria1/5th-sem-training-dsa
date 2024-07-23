5b310563fc1efab42acad2d9cba9115a2f4d3ec6
Prachi Khajuria <122385166+prachikhajuria1@users.noreply.github.com>
2024-07-23 11:55:50 +0530

    Add files via upload

---
+++ 104quicksortdemo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int>arr={10,7,8,9,1,5};
+    sort(arr.rbegin(),arr.rend());
+    cout<<"Sorted array: ";
+    for(int num:arr){
+    cout<<num<<" ";
+    }
+    return 0;
+    }
