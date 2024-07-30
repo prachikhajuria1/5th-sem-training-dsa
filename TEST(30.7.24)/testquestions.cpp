8b72fb30f22a27de215791af7511a3b9a37e0d8b
Prachi Khajuria <122385166+prachikhajuria1@users.noreply.github.com>
2024-07-30 14:12:41 +0530

    Add files via upload

---
+++ TEST(30.7.24)/testquestions.cpp
@@ -0,0 +1,10 @@
+
+
+https://leetcode.com/problems/house-robber/description/
+https://leetcode.com/problems/flipping-an-image/description/
+https://leetcode.com/problems/search-a-2d-matrix/d
+https://leetcode.com/problems/jewels-and-description/stones/description/
+https://www.naukri.com/code360/problems/reverse-the-string_799927
+https://leetcode.com/problems/sorting-the-sentence/description/
+
+
